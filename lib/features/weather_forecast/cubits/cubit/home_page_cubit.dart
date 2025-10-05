ca643a7c3795884972f4d1b37679c7d1e981c103
Oltea Negru <olteanegru.uk@gmail.com>
2025-10-05 17:53:42 +0100

    chore: use shared pref to cache last location

--- lib/features/weather_forecast/cubits/cubit/home_page_cubit.dart
+++ lib/features/weather_forecast/cubits/cubit/home_page_cubit.dart
@@ -3,20 +3,29 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:moist_crumb/features/weather_forecast/models/weather_data.dart';
 import 'package:moist_crumb/services/api/errors.dart';
 import 'package:moist_crumb/services/api/weather_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'home_page_state.dart';
 part 'home_page_cubit.freezed.dart';
 
 class HomePageCubit extends Cubit<HomePageState> {
   WeatherApi? _weatherApi;
+  static const _lastCityKey = 'last_city';
 
   HomePageCubit() : super(const HomePageState.initial()) {
     _initialize();
-  }
+  } 
 
   Future<void> _initialize() async {
     try {
       _weatherApi = WeatherApi();
+      final prefs = await SharedPreferences.getInstance();
+      final lastCity = prefs.getString(_lastCityKey);
+      if (lastCity != null && lastCity.isNotEmpty) {
+        getWeather(lastCity);
+      } else {
+        emit(const HomePageState.initial());
+      }
     } catch (e) {
       emit(HomePageState.error('', WeatherApiInitializationException(), null));
     }
@@ -26,6 +35,9 @@ class HomePageCubit extends Cubit<HomePageState> {
     emit(HomePageState.loading(city));
 
     try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_lastCityKey, city);  
+
       final weather = await _weatherApi!.getWeather(city);
       emit(HomePageState.loaded(city, weather));
     } on WeatherAPIException catch (e) {
